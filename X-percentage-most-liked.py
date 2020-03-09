d9847cb7fb6124f708b84e15525ac914770ad7d5
arthuranst <56081239+arthuranst@users.noreply.github.com>
2020-03-09 14:31:25 +0100

    Add files via upload

--- X-percentage-most-liked.py
+++ X-percentage-most-liked.py
@@ -4,12 +4,11 @@ from math import ceil
 from datetime import datetime
 from itertools import dropwhile, takewhile
 
-from instaloader import Instaloader, Profile as il
-l = il.Instaloader(dirname_pattern="/Users/arthuranst/Desktop/memoire/DL-PATH")
+from instaloader import Instaloader, Profile, Post
+l = Instaloader(dirname_pattern="/Users/arthuranst/Desktop/memoire/DL-PATH")
 l.download_post(post, filename)
 
-
-PROFILE = 'arthuranst'
+USERSPROFILE = 'arthuranst'
 X_percentage = 10   
 
 L = Instaloader()
@@ -17,13 +16,13 @@ L = Instaloader()
 SINCE = datetime(2020, 9, 1)
 UNTIL = datetime(2018, 9, 1)
 
-profile = Profile.from_username(L.context, PROFILE)
+profile = Profile.from_username(L.context, USERSPROFILE)
 posts_sorted_by_likes = sorted(profile.get_posts(),
                                key=lambda p: p.likes + p.comments,
                                reverse=True)
 
 for post in islice(posts_sorted_by_likes, ceil(profile.mediacount * X_percentage / 100)):
-    if (post.datetime <= UNTIL ):
-        L.download_post(post, PROFILE)
+    if (post.datetime <= SINCE and post.datetime >= UNTIL):
+        L.download_post(post, USERSPROFILE)
 
     
